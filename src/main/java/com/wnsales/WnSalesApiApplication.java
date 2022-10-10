d7a3708275f6286f1989966e8c1531d204344c88
Wenceslau Neto <wenceslau.neto@metyis.com>
2022-10-10 22:44:27 +0100

    --Project

---
+++ src/main/java/com/wnsales/WnSalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.wnsales.wnsalesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnSalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WnSalesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wnsales/WnSalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wnsales.wnsalesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WnSalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
